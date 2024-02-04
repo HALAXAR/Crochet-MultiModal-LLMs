28704bdd8ea009d2c11b29f44e39efa6541b63d9
Divyansh Goyal <divital2004@gmail.com>
2024-02-04 19:59:11 +0530

    Update vision_vnew.py

--- vision_vnew.py
+++ vision_vnew.py
@@ -7,13 +7,15 @@ local_api_key =  os.getenv("GOOGLE_API_KEY")
 
 file_path = "F:\example-bill.jpeg"
 
-
+#creating a class for Gemini model which encapsulates individual function definitions in the global scope
+#here the the term self refers to the object instantiated in the future 
 
 class Gemini_model:
+    #when we call the class Gemini_model() we need to give generated_api_key and file_path as arguments
     def __init__(self, generated_api_key, file_path):
-        self.apikey = generated_api_key
-        self.filepath = file_path
-        self.genai = genai.configure(api_key=generated_api_key)
+        self.apikey = generated_api_key #here we set the new object property apikey from the argument generated_api_key
+        self.filepath = file_path #setting new object property filepath from the argument file_path
+        self.genai = genai.configure(api_key=generated_api_key) #here setting the genai property after configuring the genai model from the passed api key
 
     def welcome_statement(self):
         print("-"*10+"GEMINI-VISION-APPLICATION"+"-"*10)        
@@ -38,6 +40,7 @@ class Gemini_model:
         print(self.currentresponse)
     
 
+#now as we instantiate stuff, the gemini_instance become the term "self" we used earlier in the class
 gemini_instance = Gemini_model(local_api_key, file_path)
 gemini_instance.welcome_statement()
 gemini_instance.load_image()
